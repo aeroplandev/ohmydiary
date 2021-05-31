1b3c4dbd253399968f8641792430efec77536b7e
aeroplan <aeroplan@kakao.com>
2021-05-31 10:42:51 +0900

    boot admin, security, httptrace

--- src/main/java/omds/web/WebConfig.java
+++ src/main/java/omds/web/WebConfig.java
@@ -1,14 +1,21 @@
 package omds.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
     }
 }
